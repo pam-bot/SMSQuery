39155b7e25c867c507e9b34cd5a876ce46bbd0d7
pamela-wu <pamela.wu@nyumc.org>
2014-11-17 01:33:42 -0500

    mod haversine, NESW

--- geoCoding.py
+++ geoCoding.py
@@ -5,8 +5,6 @@ import MySQLdb
 
 
 def dbLocs():
-	# cols is comma-deliminated string of columns
-	# search is the name of the affected region
 	db = MySQLdb.connect(host='localhost', user='ping', passwd='temp', db='smsante')
 	cur = db.cursor()
 	cur.execute("SELECT location, latitude, longitude, type FROM outbreaks WHERE presence='Y';")
@@ -56,19 +54,19 @@ def haversine((lat1, lng1), (lat2, lng2)):
 def NESW(br):
 	if br >= 355 or br <= 5:
 		return 'N'
-	elif br > 5 or br < 85:
+	elif br > 5 and br < 85:
 		return 'NE'
-	elif br >= 85 or br <= 95:
+	elif br >= 85 and br <= 95:
 		return 'E'
-	elif br > 95 or br < 175:
+	elif br > 95 and br < 175:
 		return 'SE'
-	elif br >= 175 or br <= 185:
+	elif br >= 175 and br <= 185:
 		return 'S'
-	elif br > 185 or br < 265:
+	elif br > 185 and br < 265:
 		return 'SW'
-	elif br >= 265 or br <= 275:
+	elif br >= 265 and br <= 275:
 		return 'W'
-	elif br > 275 or br < 355:
+	elif br > 275 and br < 355:
 		return 'NW'
 
 
@@ -90,7 +88,7 @@ def checkCoords(locInput):
 			dcollect.append(distance)
 			outbreaks[ob]['dist'] = distance
 			outbreaks[ob]['dir'] = NESW(bearing)
-		ind = dcollect.index(max(dcollect))
+		ind = dcollect.index(min(dcollect))
 		matchLoc = obKeys[ind]
 		matchLevel = outbreaks[matchLoc]['type']
 		matchDist = outbreaks[matchLoc]['dist']
